9466aa75d55102338cc64710b5e93a47daf510f0
CodeDann <sc20jdpn@leeds.ac.uk>
2024-02-16 16:29:12 +0000

    removed unused files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
 var tracker = artifacts.require("ExpenseTracker");
-// var regulator = artifacts.require("Regulator");
 
 module.exports = function(deployer) {
   deployer.deploy(tracker);
-  // deployer.deploy(regulator);
-
 };
